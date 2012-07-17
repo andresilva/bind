f92fb65bcfc1cca07a82bd3ab70ba4fee381a5a8
Andre Silva <andre.beat@gmail.com>
2012-07-17 22:01:27 +0100

    ignore whitespace

--- main.hs
+++ main.hs
@@ -1,5 +1,5 @@
 module Main where
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
 main = do
@@ -10,6 +10,9 @@ symbol :: Parser Char
 symbol = oneOf "+-*/%<>=&|^_~#:?$!@"
 
 expr :: String -> String
-expr input = case parse symbol "scheme" input of
+expr input = case parse (lexeme >> symbol) "scheme" input of
   Left err -> show err
   Right val -> show val
+
+spaces :: Parser ()
+spaces = skipMany1 space
