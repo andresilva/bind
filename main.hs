946c90798ddf575b5164eb8b04d8fb49270c324d
Andre Silva <andre.beat@gmail.com>
2012-07-18 00:01:30 +0100

    Atoms and numbers

--- main.hs
+++ main.hs
@@ -1,31 +1,57 @@
 module Main where
-import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
+import qualified Text.ParserCombinators.Parsec as Parsec
+import Text.ParserCombinators.Parsec (Parser, (<|>), char,
+                                      digit, letter, many, many1,
+                                      noneOf, oneOf, parse, skipMany1,
+                                      space)
 
 main :: IO ()
 main = do
   line <- getLine
-  putStrLn $ expr line
+  putStrLn $ eval line
 
-symbol :: Parser Char
-symbol = oneOf "+-*/%<>=&|^_~#:?$!@"
-
-expr :: String -> String
-expr input = case parse (spaces >> symbol) "scheme" input of
+eval :: String -> String
+eval input = case parse expr "scheme" input of
   Left err -> show err
   Right val -> show val
 
 spaces :: Parser ()
 spaces = skipMany1 space
 
+symbol :: Parser Char
+symbol = oneOf "+-*/%<>=&|^_~#:?$!@"
+
 data Val = Atom String
-         | List [LispVal]
-         | DottedList [LispVal] LispVal
+         | List [Val]
+         | DottedList [Val] Val
          | Number Integer
          | String String
          | Bool Bool
+         deriving (Show)
 
 string :: Parser Val
 string = do char '"'
-            x <- many (noneOf "\"")
+            x <- many (noneOf("\""))
             char '"'
             return $ String x
+
+atom :: Parser Val
+atom = do first <- letter <|> symbol
+          rest <- many (letter <|> digit <|> symbol)
+          let atom = first : rest
+          return $ case atom of
+            "#t" -> Bool True
+            "#f" -> Bool False
+            otherwise -> Atom atom
+
+number :: Parser Val
+-- number = do
+  -- n <- many1 digit
+  -- return (Number . read $ n)
+-- number = many1 digit >>= return . Number . read
+number = liftM (Number . read) $ many1 digit
+
+
+expr :: Parser Val
+expr = atom <|> string <|> number
