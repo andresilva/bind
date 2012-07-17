14b01237569ecdb707e16192de6b962b804f0032
Andre Silva <andre.beat@gmail.com>
2012-07-17 22:41:01 +0100

    scheme values, and string parser

--- main.hs
+++ main.hs
@@ -10,9 +10,22 @@ symbol :: Parser Char
 symbol = oneOf "+-*/%<>=&|^_~#:?$!@"
 
 expr :: String -> String
-expr input = case parse (lexeme >> symbol) "scheme" input of
+expr input = case parse (spaces >> symbol) "scheme" input of
   Left err -> show err
   Right val -> show val
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+data Val = Atom String
+         | List [LispVal]
+         | DottedList [LispVal] LispVal
+         | Number Integer
+         | String String
+         | Bool Bool
+
+string :: Parser Val
+string = do char '"'
+            x <- many (noneOf "\"")
+            char '"'
+            return $ String x
